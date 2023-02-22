a0148ddad44da8b1562427c5868e2407e2296e87
Dasun Thathsara <dasun.thathsara.sri@gmail.com>
2023-02-22 14:37:23 +0530

    Set Greeting Options

---
+++ main.py
@@ -0,0 +1,15 @@
+data = {"question": ["hey"], "respond": ["hey!"]}
+while True:
+    input_string = input()
+    if input_string.lower() in data["question"]:
+        print(data["respond"][data["question"].index(input_string.lower())])
+    else:
+        data["question"].append(input_string.lower())
+
+        answer = input("Sorry I don't know about that. Can you give a any suggestion for the answer: ")
+        data["respond"].append(answer)
+        # print(data)
+
+
+
+
