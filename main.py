615266035534fc30baeaba9fa7ef63ed75c58924
Dasun Thathsara <dasun.thathsara.sri@gmail.com>
2023-02-22 20:48:10 +0530

    Update the code into function base

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
-data_file = open("data.txt", 'r')
+def get_input():
+    input_string = input(">> ").lower()
+    if input_string == "i want to exit from the chat":
+        return "i want to exit from the chat"
 
-data = {"question": ["hey"], "respond": ["hey!"]}
-while True:
-    input_string = input()
-    if input_string.lower() in data["question"]:
-        print(data["respond"][data["question"].index(input_string.lower())])
+    find_solution(input_string)
+
+
+def find_solution(string):
+
+    if string in data["question"]:
+        print(">>", data["respond"][data["question"].index(string)])
     else:
-        data["question"].append(input_string.lower())
+        data["question"].append(string)
 
         answer = input("Sorry I don't know about that. Can you give a any suggestion for the answer: ")
         data["respond"].append(answer)
         # print(data)
+
+
+if __name__ == "__main__":
+    data_file = open("data.csv", 'r')
+    data = {"question": [], "respond": []}
+
+    while True:
+        if get_input() == "i want to exit from the chat":
+            print(">> Okay, see you again...")
+            break
