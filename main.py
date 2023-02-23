144bea61a0ef5e8bcac48ed20c923ac6a9f3e4d6
Dasun Thathsara <dasun.thathsara.sri@gmail.com>
2023-02-23 09:20:59 +0530

    Add store function

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# Get the user input
 def get_input():
     input_string = input(">> ").lower()
     if input_string == "i want to exit from the chat":
@@ -6,8 +7,8 @@ def get_input():
     find_solution(input_string)
 
 
+# Find the better solution for the given question using dataset
 def find_solution(string):
-
     if string in data["question"]:
         print(">>", data["respond"][data["question"].index(string)])
     else:
@@ -15,14 +16,24 @@ def find_solution(string):
 
         answer = input("Sorry I don't know about that. Can you give a any suggestion for the answer: ")
         data["respond"].append(answer)
-        # print(data)
+
+
+# Store the current values
+def store_data():
+    for i in range(len(data["question"])):
+        data_file.write(data["question"][i] + ", " + data["respond"][i])
+        data_file.write("\n")
 
 
 if __name__ == "__main__":
-    data_file = open("data.csv", 'r')
+    data_file = open("data.csv", 'w')
     data = {"question": [], "respond": []}
+
+    # for items in data_file:
 
     while True:
         if get_input() == "i want to exit from the chat":
             print(">> Okay, see you again...")
             break
+
+        store_data()
